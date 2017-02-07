4999d5628ebe56c04ecc97059cd392be0e5c1544
Thanh Nguyen <thanhnv.ip@gmail.com>
2017-02-07 18:21:38 +0700

    Initial Commit

---
+++ ATMDiary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ATMDiary
+//
+//  Created by  on 2/7/17.
+//  Copyright © 2017 thanhnv. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
